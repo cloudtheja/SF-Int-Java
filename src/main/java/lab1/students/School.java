cb59ad8d4ef7155c405878c188032bbecb103167
Simon <simon@dancingcloudservices.com>
2023-09-20 08:59:19 -0600

    Day 2 break 2

--- src/main/java/lab1/students/School.java
+++ src/main/java/lab1/students/School.java
@@ -7,16 +7,31 @@ interface CriterionOfStudent {
 }
 
 class SmartStudent implements CriterionOfStudent {
+  private double threshold;
+
+  public SmartStudent(double threshold) {
+    this.threshold = threshold;
+  }
+
   @Override
   public boolean test(Student s) {
-    return s.getGpa() > 3.0;
+    return s.getGpa() > threshold;
+  }
+}
+
+class EnthusiasticStudent implements CriterionOfStudent {
+  @Override
+  public boolean test(Student s) {
+    return s.getCourses().size() > 2;
   }
 }
 
 public class School {
+  // OO patterns call this "Command" pattern
+  // functional programming simply calls this (and other things) "Higher Order Function"
   public static void showByCriterion(List<Student> ls, CriterionOfStudent crit) {
     for (var s : ls) {
-      if (crit.test(s)) {
+      if (crit.test(s)) { // NOT crit(s)
         System.out.println("Interesting! " + s);
       }
     }
@@ -55,11 +70,28 @@ public class School {
 //    showSmart(school, 3.0);
 //    showSmart(school);
 
-    showByCriterion(school, new SmartStudent());
+    CriterionOfStudent smarterThanThree = new SmartStudent(3.0);
+    showByCriterion(school, smarterThanThree);
     System.out.println("------------");
 //    showSmart(school, 2.0);
+    showByCriterion(school, new SmartStudent(2.0));
     System.out.println("------------");
 //    showEnthusiastic(school);
+    showByCriterion(school, new EnthusiasticStudent());
+    System.out.println("------------");
+
+    // we KNOW (and so does the compiler) this second argument MUST BE
+    // CriterionOfStudent
+    // we also know that interface declares ONE abstract method
+    // which takes a single argument, of type Student, and
+    // returns boolean
+    // IF (but only if) we only wish to provide that one abstract method
+    // we could (conceivably) define the method, and have the compiler
+    // a) build a (hidden) class around that method
+    // b) instantiate it
+    showByCriterion(school,
+      (Student s) -> { return s.getCourses().size() <= 2; }
+    );
 
   }
 }
